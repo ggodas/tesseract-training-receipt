9e9c0e153644ce403099404ee8c2ce2ff38d1619
George Godas <george.godas@ftccards.com.br>
2015-08-27 13:34:47 -0300

    Printer epson trained Printer zantus trained

--- training.sh
+++ training.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#java -Xms128m -Xmx1024m -jar jTessBoxEditor.jar
 #convert -density 300 -depth 4 lang.font-name.exp0.pdf lang.font-name.exp0.tif
 tesseract lang.font-name.exp0.tiff lang.font-name.exp0 batch.nochop makebox
 
@@ -7,7 +8,7 @@ tesseract ftc.ibm.exp0.tif ftc.ibm.exp0.box batch.nochop makebox
 
 tesseract ftc.bematech.exp0.tif ftc.bematech.exp0.box box.train.stderr
 tesseract ftc.sweda.exp0.tif ftc.sweda.exp0.box box.train.stderr
-unicharset_extractor ftc.bematech.exp0.box ftc.sweda.exp0.box
+unicharset_extractor *.box
 
 
 shapeclustering -F ftc.font_properties -U unicharset *.tr
